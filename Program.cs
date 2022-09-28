9ddffe4abda4f1d73212fe662d7c0ac3f5209fe6
Vlad Trofimchuk <vladtrofimchyk@gmail.com>
2022-09-28 22:58:17 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Яке найменше додатне число ділиться на всі числа від 1 до 10?
+
+namespace Ivan
+{
+    internal class Program
+    {
+
+        static void Krat(int a)
+        {
+            bool start = true;
+            ulong count = 0;
+            while (start)
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    if (a % i != 0)
+                    {
+                        Krat(a + 1);
+                    }
+                    else
+                    {
+                        count++;
+                    }
+
+                    if (count == 10)
+                    {
+                        start = false;
+                        Console.WriteLine($"{a} мега число");
+                        Console.ReadKey();
+                    }
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Krat(1);
+
+        }
+    }
+}
